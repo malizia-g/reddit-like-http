3462472300385f7a8f02d549ad64e4c4192d4f9b
EC2 Default User <ec2-user@ip-172-31-6-126.ec2.internal>
2019-04-15 10:13:09 +0000

    reddit

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, HostBinding, Input } from '@angular/core'; //import Input
+import { Article } from './article.model'; //import article
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'card';
+  @Input() article:Article
+
+  constructor() {
+    //L'article è popolato dall'@Input
+  }
+
+  voteUp(): Boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown():Boolean {
+    this.article.voteDown();
+    return false; 
+  }
+
+  ngOnInit() {}
+}
